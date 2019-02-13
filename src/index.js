800f69e6784ce2d488c7ba420d765f6db4a2c155
Utku Kaan <hackerhutku001@gmail.com>
2019-02-13 04:58:29 +0300

    Setup Register page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
-import "./index.css";
+import "semantic-ui-css/semantic.min.css";
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
