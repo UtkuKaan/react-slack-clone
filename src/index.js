d55ee4702a5d828e49349149fc8bb89259250f5f
Utku Kaan <hackerhutku001@gmail.com>
2019-02-16 00:32:07 +0300

    Set up Redux store

--- src/index.js
+++ src/index.js
@@ -6,17 +6,25 @@ import {
   Route,
   withRouter
 } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import firebase from "./firebase";
 import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
+import rootReducer from "./reducers";
+import { setUser } from "./actions";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
+        this.props.setUser(user);
         this.props.history.push("/");
       }
     });
@@ -32,12 +40,19 @@ class Root extends React.Component {
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(
+  connect(
+    null,
+    { setUser }
+  )(Root)
+);
 
 ReactDOM.render(
-  <Router>
-    <RootWithAuth />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
---
+++ src/reducers/index.js
@@ -0,0 +1,25 @@
+import { combineReducers } from "redux";
+import * as actionTypes from "../actions/types";
+
+const initialUserState = {
+  currentUser: null,
+  isLoading: true
+};
+
+const userReducer = (state = initialUserState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_USER:
+      return {
+        currentUser: action.payload.currentUser,
+        isLoading: false
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  user: userReducer
+});
+
+export default rootReducer;
