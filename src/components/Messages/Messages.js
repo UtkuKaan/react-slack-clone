a017dcd3f3c261a16d45f05bf0b602f6ab36233c
Utku Kaan <hackerhutku001@gmail.com>
2019-02-27 18:32:49 +0300

    Display messages for current channel

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -2,14 +2,49 @@ import React from "react";
 import { Segment, Comment } from "semantic-ui-react";
 import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
+import Message from "./Message";
 import firebase from "../../firebase";
 
 class Messages extends React.Component {
   state = {
     messagesRef: firebase.database().ref("messages"),
+    messages: [],
+    messagesLoading: true,
     channel: this.props.currentChannel,
     user: this.props.currentUser
   };
+
+  componentDidMount() {
+    if (this.state.channel && this.state.user) {
+      this.addListeners(this.state.channel.id);
+    }
+  }
+
+  addListeners = channelId => {
+    this.addMessageListener(channelId);
+  };
+
+  addMessageListener = channelId => {
+    let loadedMessages = [];
+
+    this.state.messagesRef.child(channelId).on("child_added", snap => {
+      loadedMessages.push(snap.val());
+      this.setState({
+        messages: loadedMessages,
+        messagesLoading: false
+      });
+    });
+  };
+
+  displayMessages = messages =>
+    messages.length > 0 &&
+    messages.map(message => (
+      <Message
+        key={message.timestamp}
+        message={message}
+        user={this.state.user}
+      />
+    ));
 
   render() {
     return (
@@ -17,7 +52,9 @@ class Messages extends React.Component {
         <MessagesHeader />
 
         <Segment>
-          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+          <Comment.Group className="messages">
+            {this.displayMessages(this.state.messages)}
+          </Comment.Group>
         </Segment>
 
         <MessageForm
